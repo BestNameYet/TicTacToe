f1430d1c7a38e10d9964a13395b562fd652e089b
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-19 23:20:20 -0500

    Update model.js

--- model.js
+++ model.js
@@ -1,12 +1,17 @@
 window.player = {
   isHuman: true,
-  symbol: 'X',
+  symbol: 1,
   switch: function(){
     this.isHuman = !this.isHuman
     if(this.isHuman){
-      this.symbol = 'X';
+      this.symbol = -1;
     }
     else
-      this.symbol = 'Y';
+      this.symbol = 1;
   } 
+}
+
+window.board = new Map();
+for(var i = 0; i < 10; i++){
+  board.set(i, 0);
 }
