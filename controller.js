3c044dd610afcb2b55634ee4f7e7190579a3f95c
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-23 02:13:28 -0500

    Update controller.js

--- controller.js
+++ controller.js
@@ -8,31 +8,31 @@ class Controller{
   }
 
   updateBoardView(){
-   for(let i = 0; i<boardModel.length; i++){
-      if(boardModel[i].type == "human"){
-         boardView[i].value = "X";
+   for(let i = 0; i<this.boardModel.length; i++){
+      if(this.boardModel[i].type == "human"){
+         this.boardView[i].value = "X";
       }
-      else if(boardModel[i].type == "computer"){
-         boardView[i].value = "O";
+      else if(this.boardModel[i].type == "computer"){
+         this.boardView[i].value = "O";
       }
-      else if(boardModel[i].type == "blank"){
-         boardView[i].value = "";
+      else if(this.boardModel[i].type == "blank"){
+         this.boardView[i].value = "";
       }
     }
   }
 
   updateBoardModel(choice){
    let position = Number(choice.id);
-   let valid = validMove(boardModel);
+   let valid = validMove(this.boardModel);
    if(valid){
-      boardModel[position] = this.player;
+      this.boardModel[position] = this.player;
       this.updateBoardView();
       if(player.getWinner(board)){
          this.updateWinnerView();
       }
       else {
          this.updateTurnInfoView();
-         this.player = new Player(player.opponentType);
+         this.player = new Player(this.player.opponentType);
       }
          
       
@@ -43,15 +43,15 @@ class Controller{
 
   updateTurnInfoView(){
      if(this.player.opponentType == "human"){
-        turnView.innerHTML = "X's turn";
+        this.turnView.innerHTML = "X's turn";
      }
      else turnView.innerHTML = "O's turn";
   }
   updateWinnerView(){
      if(this.player.type == "human"){
-        turnView.innerHTML = "X wins!";
+        this.turnView.innerHTML = "X wins!";
      }
-     else turnView.innerHTML = "O wins!";
+     else this.turnView.innerHTML = "O wins!";
   }
 
   newGameView(){
