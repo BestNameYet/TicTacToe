cf556717c599ef1fefbd5470a539a48e1c2b84c4
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-23 02:08:40 -0500

    Update controller.js

--- controller.js
+++ controller.js
@@ -4,15 +4,54 @@ class Controller{
     this.boardModel = boardModel;
     this.boardView = boardView;
     this.turnView = turnView;
+    this.player = new Player("human");
   }
 
   updateBoardView(){
+   for(let i = 0; i<boardModel.length; i++){
+      if(boardModel[i].type == "human"){
+         boardView[i].value = "X";
+      }
+      else if(boardModel[i].type == "computer"){
+         boardView[i].value = "O";
+      }
+      else if(boardModel[i].type == "blank"){
+         boardView[i].value = "";
+      }
+    }
   }
 
-  updateBoardModel(){
+  updateBoardModel(choice){
+   let position = Number(choice.id);
+   let valid = validMove(boardModel);
+   if(valid){
+      boardModel[position] = this.player;
+      this.updateBoardView();
+      if(player.getWinner(board)){
+         this.updateWinnerView();
+      }
+      else {
+         this.updateTurnInfoView();
+         this.player = new Player(player.opponentType);
+      }
+         
+      
+   }
+   else
+      alert('That square has already been played.');
   }
 
   updateTurnInfoView(){
+     if(this.player.opponentType == "human"){
+        turnView.innerHTML = "X's turn";
+     }
+     else turnView.innerHTML = "O's turn";
+  }
+  updateWinnerView(){
+     if(this.player.type == "human"){
+        turnView.innerHTML = "X wins!";
+     }
+     else turnView.innerHTML = "O wins!";
   }
 
   newGameView(){
