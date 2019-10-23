5705d7bcad7258ce8638b203ee4c89439471bd20
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-23 02:39:58 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -10,4 +10,67 @@ const turnInfoView = document.getElementById('status');
 
 const newGameButtonView = document.getElementById('newGame');
 
+class Controller{
+   
+  constructor(boardModel, boardView, turnView){
+    this.boardModel = boardModel;
+    this.boardView = boardView;
+    this.turnView = turnView;
+    this.player = new Player("human");
+  }
+
+  updateBoardView(){
+   for(let i = 0; i<this.boardModel.length; i++){
+      if(this.boardModel[i].type == "human"){
+         this.boardView[i].value = "X";
+      }
+      else if(this.boardModel[i].type == "computer"){
+         this.boardView[i].value = "O";
+      }
+      else if(this.boardModel[i].type == "blank"){
+         this.boardView[i].value = "";
+      }
+    }
+  }
+
+  updateBoardModel(choice){
+   let position = Number(choice.id);
+   let valid = validMove(this.boardModel, position);
+   if(valid){
+      this.boardModel[position] = this.player;
+      this.updateBoardView();
+      if(player.getWinner(board)){
+         this.updateWinnerView();
+      }
+      else {
+         this.updateTurnInfoView();
+         this.player = new Player(this.player.opponentType);
+      }
+         
+      
+   }
+   else
+      alert('That square has already been played.');
+  }
+
+  updateTurnInfoView(){
+     if(this.player.opponentType == "human"){
+        this.turnView.innerHTML = "X's turn";
+     }
+     else turnView.innerHTML = "O's turn";
+  }
+  updateWinnerView(){
+     if(this.player.type == "human"){
+        this.turnView.innerHTML = "X wins!";
+     }
+     else this.turnView.innerHTML = "O wins!";
+  }
+
+  newGameView(){
+  }
+
+  newGameModel(){
+  }
+}
+
 const controller = new Controller(board, boardView, turnInfoView);
