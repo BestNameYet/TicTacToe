596918eeba96c49f96325e8f9ce43386b76c46ff
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-23 02:39:19 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,1 +1,13 @@
+const boardView = (function(){
+  var array = [];
+  for(let i = 0; i < 9; i++){
+    array.push(document.getElementById(Number(i).toString));
+  }
+  return array;
+})();
+
+const turnInfoView = document.getElementById('status');
+
+const newGameButtonView = document.getElementById('newGame');
+
 const controller = new Controller(board, boardView, turnInfoView);
