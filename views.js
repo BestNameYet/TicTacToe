b0bf9cb523060e21e4b348866aa4a0a19d575c23
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-22 21:55:47 -0500

    Create views.js

---
+++ views.js
@@ -0,0 +1,7 @@
+const boardView = [document.getElementById('0'),document.getElementById('1'),document.getElementById('2'),
+document.getElementById('3'),document.getElementById('4'),document.getElementById('5'),
+document.getElementById('6'),document.getElementById('7'),document.getElementById('8')];
+
+const turnInfoView = document.getElementById('status');
+
+const newGameButtonView = document.getElementById('newGame');
