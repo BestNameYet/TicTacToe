1ac4b5100ac9300e7adf310fcfae5a46a4e3b3a6
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-23 00:24:39 -0500

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,1 +1,19 @@
+function getWinner(board){
+}
 
+function updateBoardView(board){
+}
+
+function updateTurnInfoView(player){
+}
+
+function newGameView(){
+}
+
+function newGameModel(){
+}
+
+function minimax(board, player){
+}
+                       
+                   
