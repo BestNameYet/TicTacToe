5bc8c9ba7416c80ea506317c526b92f72f74abe6
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-24 12:19:17 -0500

    Update model.js

--- model.js
+++ model.js
@@ -2,25 +2,35 @@ class TTTModel{
   
   xPlayer = new Player();
   oPlayer = new Player();
+  whoWaititng = null;
+  whoPlaying = null;
   board  = new Board();
   moveStack = [];
-  moveCounter = 0;
+  moveCount = 0;
   isPaused = true;
   isGameOver = false;
   hasWon = false;
   hasDraw = false;
+  whoWinner = null;
+  whoLoser = null;
     
   constructor(){
   }
   
   
   makeMove(positionNumber){
+    if(this.isValidMove(positionNumber)){
+      this.
+    }
   }
   
   undoLastMove(){
   }
   
   resetMatch(){
+  }
+
+  switchPlayers(){
   }
   
   unpauseMatch(){
@@ -30,96 +40,169 @@ class TTTModel{
   }
     
   get positions(){
+    
   }
   
-  set position(position, type){
+  set position(positionNumber){
+    if(this.whoPlaying != null){
+      this.board.position(positionNumber, this.whoPlaying);
+      return true;
+    }
+    else return false;
   }
   
   get moveCount(){
+    return this.moveCount;
   }
   
   get isValidMove(positionNumber){
+    let positions = this.board.positions;
+    if(positions[positionNumber] == null){
+      return true;
+    }
+    else return false;
   }
   
   get moveMinimaxScore(positionNumber){
   }
   
   get hasGameOver(){
+    return this.isGameOver;
   }
   
   get hasWin(){
+    return this.hasWon;
   }
   
   get hasDraw(){
+    return this.hasDraw;
   }
   
   get whoWinner(){
+    if(whoWinner != null){
+      return JSON.stringify(this.whoWinner.info);
+    }
+    else return false;
+    
   }
   
   get whoLoser(){
+    if(whoLoser != null){
+      return JSON.stringify(this.whoLoser.info);
+    }
+    else return false;
   } 
   
   get whoPlayingType(){
+    if(whoPlaying != null){
+      return this.whoPlaying.type.toUpperCase();
+    }
+    else return false;
   }
   
   get whoWaitingType(){
+    if(whoWaiting != null){
+      return this.whoWaiting.type.toUpperCase();
+    }
+    else return false;
   }
   
   get whoPlayingName(){
+    if(whoPlaying != null){
+      return this.whoPlaying.name.toUpperCase();
+    }
+    else return false;
+  }
+
+  get whoWaitingName(){
+    if(whoWaiting != null){
+      return this.whoWaiting.name.toUpperCase();
+    }
+    else return false;
   }
   
-  get whoWaitingName(){
+  get whoPlayingMark(){
+    if(this.whoPlaying != null){
+      return this.whoPlaying.maek.toUpperCase();
+    }
+    else return false;
   }
+
+  get whoWaitingMark(){
+    if(this.whoWaiting != null){
+      return this.whoWaiting.mark.toUpperCase();
+    }
+    else return false;
+  }
+  
+
   
   get xPlayerType(){
+    return this.xPlayer.type;
   }
   
   set xPlayerType(type){
+    this.xPlayer.type = type;
   }
   
   get oPlayerType(){
+    return this.oPlayer.type;
   }
   
   set oPlayerType(type){
+    this.oPlayer.type = type;
   }
   
   get xPlayerName(){
+    return this.xPlayer.name;
   }
   
   set xPlayerName(name){
+    this.xPlayer.name = name;
   }
   
   get oPlayerName(){
+    return this.oPlayer.name;
   }
   
   set oPlayerName(name){
+    this.oPlayer.name = name;
   }
 }
 
 class Player{
-  type = null;
-  name = null;
-  mark = null;
+  info;
   
-  constructor(type, name, mark){
-    this.type = type;
-    this.name = name;
-    this.mark = mark;
+  constructor(info = {name: '', type: '', mark: ''}){
+    this.info = info;
+  }
+  
+  get info(){
+    return this.info;
   }
   
   set type(type){
-  
+    this.info.type = type;
   }
   
-  get type(type){
-  
+  get type(){
+    return this.info.type;
   }
   
   set name(name){
+    this.info.name = name;
   }
   
   get name(){
+    return this.info.name;
+  }
   
+  set mark(name){
+    this.info.mark = mark;
+  }
+  
+  get mark(){
+    return this.info.mark;
   }
 }
 
@@ -142,7 +225,7 @@ class Player{
     constructor(){
     }
 
-    set position(position, player){
+    set position(positionNumber, player){
     }
 
     get positions(){
