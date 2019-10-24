c1024296676d798c6c4bf10eabc1c3614af59815
BestNameYet <samuel.john.nixon@gmail.com>
2019-10-24 04:24:50 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -49,7 +49,7 @@ function minimax(board, player){
   }
   else if(opponent.getWinner(board)){
     return -1;}
-  var move = -1;
+  var ] = -1;
   var score = -2;
   for(let i = 0; i<9; i++){
     if(board[i].type == "blank"){
@@ -205,9 +205,9 @@ class Controller{
     this.turnView.style.color = "black";
     
      if(this.player.type == "human"){
-       this.turnView.innerHTML = "Move: " + move;
+       this.turnView.innerHTML = "Move: " + this.move;
      }
-     else this.turnView.innerHTML = "Move: " + move;
+     else this.turnView.innerHTML = "Move: " + this.move;
   }
   updateWinnerView(){
      if(this.player.type == "human"){
